e76f053024a0befea0490fed289ff8a94ac2fee0
Talexan <lkotov.mihaill@gmail.com>
2021-10-11 14:38:08 +0300

    Not finished, but minor fixes have been added

--- Model/Payment/Method/ConfigProvider.php
+++ Model/Payment/Method/ConfigProvider.php
@@ -6,6 +6,8 @@ class ConfigProvider implements \Magento\Checkout\Model\ConfigProviderInterface
 {
     public function getConfig()
     {
-        
+        return [
+            // 'key' => 'value' pairs of configuration
+        ];
     }
 }
