9fb6b09bbf11c215951ddbf660f6f56fcdb270ef
Talexan <lKotov.mihaill@gmail.com>
2021-10-17 10:10:54 +0300

    Done

--- Model/Payment/Method/LoyaltyCoin.php
+++ Model/Payment/Method/LoyaltyCoin.php
@@ -1,6 +1,9 @@
 <?php
 namespace Talexan\Credit\Model\Payment\Method;
 
+use Talexan\Credit\Model\CoinFactory;
+use Talexan\Credit\Model\Coin;
+
 /**
  * Pay In Store payment method model
  */
@@ -15,4 +18,117 @@ class LoyaltyCoin extends \Magento\Payment\Model\Method\AbstractMethod
      * @var string
      */
     protected $_code = self::CODE;
+
+    /**
+     * @var Talexan\Credit\Model\CoinFactory $coinFactory
+     */
+    private $_coinFactory;
+
+    /**
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory
+     * @param \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory
+     * @param \Magento\Payment\Helper\Data $paymentData
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param Logger $logger
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
+     * @param array $data
+     * @param DirectoryHelper $directory
+     * @param Talexan\Credit\Model\CoinFactory $coinFactory
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        \Magento\Payment\Helper\Data $paymentData,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Payment\Model\Method\Logger $logger,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection,
+        array $data,
+        DirectoryHelper $directory,
+        \Talexan\Credit\Model\CoinFactory $coinFactory
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData,
+            $scopeConfig,
+            $logger,
+            $resource,
+            $resourceCollection,
+            $data,
+            $directory
+        );
+
+        $this->_coinFactory = $coinFactory;
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function isAvailable(\Magento\Quote\Api\Data\CartInterface $quote = null)
+    {
+       $this->setData('quote', $quote);
+       return parent::isAvailable($quote) && ($this->_getAmountCoins($quote) >= $quote->getGrandTotal());
+    }
+
+    /**
+     * Сhecks if there are enough credit coins
+     * @param \Magento\Quote\Api\Data\CartInterface $quote
+     * @return float|null
+     */
+    private function _getAmountCoins(\Magento\Quote\Api\Data\CartInterface $quote){
+        if($quote){
+           return floatval($quote->getCustomer()->getData('customer_coins'));
+        }
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount)
+    {
+        /** @var \Magento\Sales\Model\Order $order */
+        $order = $payment->getOrder();
+
+        try{
+            //////// setAttributeCredit ////////
+            if ($this->getData('quote')){
+                $customer = $this->getData('quote')->getCustomer();
+                $customer->setData('customer_coins', $customer->getData('customer_coins')-$amount);
+                $customer->save();
+            }
+
+            //////// setHistoryCredit /////////
+            $creditHistory = $this->_coinFactory->create();
+            $creditHistory->addData([
+                'customer_id' => $order->getCustomerId(),
+                'occasion'    => $order->getRealOrderId(),
+                'amount_purchase' => $amount,
+                'coins_received' => -$amount,
+                'created_at' => $order->getCreatedAt()
+            ]);
+            $creditHistory->save();
+
+            $payment->setTransactionId($order->getRealOrderId())
+                ->setIsTransactionClosed(0);
+ 
+        } catch (\Exception $e) {
+            $this->debugData(['exception' => $e->getMessage()]);
+            $this->_logger->error(__('Payment capturing error.'));
+            throw new \Magento\Framework\Validator\Exception(__('Payment capturing error.'));
+        }
+
+        return $this;
+    }
+
 }
---
+++ Observer/DataAssignObserver.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Talexan\Credit\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use \Magento\Customer\Api\CustomerRepositoryInterface;
+use Talexan\Credit\Model\CoinFactory;
+use Talexan\Credit\Model\Coin;
+
+
+class DataAssignObserver implements ObserverInterface
+{
+    /**
+     * @var \Magento\Sales\Model\ResourceModel\Order\Collection
+     */
+    protected $_collection;
+
+    /**
+     * @var \Magento\Customer\Api\CustomerRepositoryInterface 
+     */
+    protected $_customerRepository;
+
+    /**
+     * @var Talexan\Credit\Model\CoinFactory 
+     */
+    protected $_coinFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository
+     * @param \Magento\Sales\Model\ResourceModel\Order\Collection $collection
+     * @param Talexan\Credit\Model\CoinFactory $coinFactory
+     */
+    public function __construct(
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        \Magento\Sales\Model\ResourceModel\Order\Collection $collection,
+        \Talexan\Credit\Model\CoinFactory $coinFactory
+    ) {
+        $this->_customerRepository = $customerRepository;
+        $this->_collection = $collection;
+        
+    }
+
+    /**
+     * 
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return $this
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+       
+        $orderIds = $observer->getEvent()->getOrderIds();
+        if (!$orderIds || !is_array($orderIds)) {
+            return $this;
+        }
+        
+        $this->_collection->addFieldToFilter('entity_id', ['in' => $orderIds]);
+
+        foreach ($this->_collection as $order) {
+            /** @var $order \Magento\Sales\Api\Data\OrderInterface */
+
+            $additionalData[] = unserialize($order->getAdditionalData());
+
+            if(isset($additionalData[\Talexan\Credit\Plugin\Payment::CREDIT_COINS_ENABLED]) 
+                && ($additionalData[\Talexan\Credit\Plugin\Payment::CREDIT_COINS_ENABLED] == 1)){
+                   
+                    $creditCoin = $order->getGrandTotal() * 
+                    $additionalData[\Talexan\Credit\Plugin\Payment::CREDIT_COINS_PERCENT_PURCHASE];
+
+                    //////// setAttributeCredit ////////
+                    $customer = $this->_customerRepository->getById(
+                            $order->getCustomerId());
+                    $customer->setData('customer_coins', $customer->getData('customer_coins')
+                                        + $creditCoin);
+                    $customer->save();
+
+                    //////// setHistoryCredit /////////
+                    $creditHistory = $this->_coinFactory->create();
+                    $creditHistory->addData([
+                                                'customer_id' => $order->getCustomerId(),
+                                                'occasion'    => $order->getRealOrderId(),
+                                                'amount_purchase' => $order->getGrandTotal(),
+                                                'coins_received' => $creditCoin,
+                                                'created_at' => $order->getCreatedAt()
+                                            ]);
+                    $creditHistory->save();
+                }
+        }
+       
+        return $this;
+    }
+}
