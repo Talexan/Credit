9ba4f465ce9664ba8a3f773b4ebde10b1a128717
Talexan <lkotov.mihaill@gmail.com>
2021-12-09 18:48:38 +0200

    Create payment method

--- Credit/view/frontend/web/js/view/payment/method-renderer/loyalty-coin.js
+++ Credit/view/frontend/web/js/view/payment/method-renderer/loyalty-coin.js
@@ -6,7 +6,39 @@ function ($, Component)
     return Component.extend({
         defaults: {
             template: 'Talexan_Credit/payment/loyaltycoin',
+            transactionResult: ''
         },
+
+        initObservable: function () {
+
+            this._super()
+                .observe([
+                    'transactionResult'
+                ]);
+            return this;
+        },
+
+        getCode: function() {
+            return 'Loyalty credit coins';
+        },
+
+        getData: function() {
+            return {
+                'method': this.item.method,
+                'additional_data': {
+                    'transaction_result': this.transactionResult()
+                }
+            };
+        },
+
+        getTransactionResults: function() {
+            return _.map(window.checkoutConfig.payment.loyaltycoin.transactionResults, function(value, key) {
+                return {
+                    'value': key,
+                    'transaction_result': value
+                }
+            });
+        }
 
 //       initObservable: function () {
 //
